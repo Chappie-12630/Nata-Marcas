58b1dad3d62fa0c08c1fd7e512cdccab7acc5c1c
Chappie-12630 <espirigonzalesfalsh1@gmail.com>
2026-01-05 00:11:27 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- ESTO ES VITAL: Asegura que las rutas sean relativas
+  base: './', 
 })
