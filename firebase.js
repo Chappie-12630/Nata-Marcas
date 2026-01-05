c2297dbfd96b8909725a6277cdc165812cecd377
Chappie-12630 <espirigonzalesfalsh1@gmail.com>
2026-01-04 23:43:35 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,3 +12,18 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+// src/lib/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY_AQUÍ",
+  authDomain: "tu-app.firebaseapp.com",
+  projectId: "tu-app-id",
+  storageBucket: "tu-app.appspot.com",
+  messagingSenderId: "tu-id",
+  appId: "tu-app-id"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
